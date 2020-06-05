479ca78d5c8ccf8c94a67ded14967230d751a8b9
Matthias Urlichs <matthias@urlichs.de>
2020-06-05 13:53:07 +0200

    Don't die when there's no current task
    asyncio's current_task() can be 'None'.
    weakrefs to 'None' are not possible.

--- anyio/_backends/_asyncio.py
+++ anyio/_backends/_asyncio.py
@@ -256,8 +256,11 @@ abc.CancelScope.register(CancelScope)
 
 
 def check_cancelled():
+    task = current_task()
+    if task is None:
+        return
     try:
-        cancel_scope = _task_states[current_task()].cancel_scope
+        cancel_scope = _task_states[task].cancel_scope
     except KeyError:
         return
 
